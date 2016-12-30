2f4aa7605a4bcee69c7a8f3d7119cf168d226e58
crazyzubr <hjcnbckfd@gmail.com>
2016-12-31 01:23:02 +0300

    Fix missing import in admin.py

--- mailchimp/admin.py
+++ mailchimp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from mailchimp.models import Campaign
 from mailchimp.settings import VIEWS_OVERVIEW
+import mailchimp.views
 
 
 class MailchimpAdmin(admin.ModelAdmin):
